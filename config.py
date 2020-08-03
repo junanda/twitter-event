e8f0a9b6e1b14200f16119c1af87f402ab1bbe1c
Junanda Patihullah <jpatihullah@gmail.com>
2020-08-03 23:55:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-username = "tombelote"
-consumer_key = "KCZNRp2yMpJge0wWgpcSo3jD3"
-consumer_secret = "9uhxhOWvcdembgd3SvGT6eFeDdGWLg12Sr48xEK4SVvVX8mPki"
-token_key = "50536091-KEJ4ykWfYfrtmdjIo7I3SPmtiUUIonwiNfqHjyKY4"
-token_secret = "a0ozpDsbwtO4LAmDpuTnCnTd7zOldhwMj04ePpfJbKlaD"
+username = "xxxxxxxxx"
+consumer_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+consumer_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+token_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+token_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 user_agent_old_phone = 'Nokia5310XpressMusic_CMCC/2.0 (10.10) Profile/MIDP-2.1 Configuration/CLDC-1.1 UCWEB/2.0 (Java; U; MIDP-2.0; en-US; Nokia5310XpressMusic) U2/1.0.0 UCBrowser/9.5.0.449 U2/1.0.0 Mobile'
